24bac18df6021204c84dba48b2b17724ffc78c94
joao-manoel <joaomanoel.ms.br@gmail.com>
2021-03-10 02:07:12 -0400

    removed console log

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -23,10 +23,7 @@ export function TaskList() {
       title: newTaskTitle,
       isComplete: false
     }
-
-    console.log(Math.random())
-
-    setTasks(old => [...tasks, newTask]);
+    setTasks([...tasks, newTask]);
     setNewTaskTitle('');
   }
 
